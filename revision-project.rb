d820a5c9fcb91d530e3847528f3653197dde59bd
Francesco Argentieri <francesco@MacBook-Pro-di-Francesco.local>
2022-05-15 21:31:27 +0200

    Add new feature to filter directories

--- revision-project.rb
+++ revision-project.rb
@@ -1,5 +1,97 @@
 #!/usr/bin/env ruby
 
+KNOW_FOLDER = ["app", "script", "package","autobuild","conf.d","bazel-bin", "bazel-out", "bazel-testlogs", "tests", "bazel-speech", "binding", "app_test", "thirdparty", "build", "asset", "readme-asset", "assets"]
+
+
+def filter_know_subfolder(counter, projects)
+	for subproject in projects do
+		if File.directory?(subproject) && !KNOW_FOLDER.include?(subproject.split('/').last)
+			result = subproject.split('/').last
+			puts "kkk #{result}"
+			counter += 1
+			yield(counter, result)
+		end
+	end
+end
+
+
+
+class Project
+	attr_accessor :name_, :branch_, :commit_, :tag_
+
+
+
+	def initialize(name, branch, commit, tag = "N.A.")
+		@size = 0
+		@name_ = has_subproject(name)
+		@branch_ = valid_branch(branch)
+		@commit_ = commit
+		@tag_ = tag
+	end
+
+
+	def get_project
+		if @size == 0 
+			return nil
+		end
+		if @size == 1
+			return {name: 		@name_,	branch:  	@branch_, commit: 	@commit_,	tag: 		@tag_}
+		end
+		if @size > 1
+		 	return []
+		end
+	end
+
+	def size
+		@size
+	end
+
+	def to_s
+	end
+
+
+	private
+	def valid_branch(branch)
+		branch = branch.strip
+		case branch
+		when "main"
+			return branch
+		when "master"
+			return branch
+		when "develop"
+		 	return branch
+		when /feature\/.*/
+			return branch
+		when /hotfix\/.*/
+			return branch
+		when /release\/.*/
+			return branch
+		when /experiment.*/
+			return branch
+		else
+			raise "Not found a valid branch name"
+		end
+	end
+
+	def has_subproject(name)
+		out = nil
+		subfolders = Dir.glob([name + "/**"])
+		filter_know_subfolder(@size, subfolders){
+			|i, res| puts "kuri #{i}, #{res}"
+			@size = i
+			if i == 1
+				out = name.split('/').last
+			elsif i > 1
+				out << res
+			else
+				out = res
+			end
+
+		}
+		out
+	end
+end
+
 
 class ProjectRevision 
 
@@ -9,6 +101,7 @@ class ProjectRevision
 		time = Time.new
 		@timestamp = time.strftime("%d-%m-%Y")
 		@projects_directory_ = project_directory
+		@repository_projects_ = []
 		@valid_projects_ = []
 	end
 
@@ -18,22 +111,22 @@ class ProjectRevision
 		for folder in folders do
 			if File.directory?(folder+"/.git")
 				puts "==> found valid git project: #{folder}"
-				@valid_projects_ << folder
+				@repository_projects_ << folder
 			end
 		end
 	end
 
 	def register_file_on_disk(filename = "report")
-		# validate exestenison
+		# validate extension
 		if @@ACCEPTED_FORMATS.include? File.extname(filename)
-			# is valid nothing to do
+			# filename is valid nothing to do
 		else 
 			puts "==> #{filename} is missing extension or unsupported, then utilise the default extension"
 			filename = filename + ".md"
 		end
 		
-		filename = File.join(Dir.pwd, filename)
 		# register the file on disk
+		filename = File.join(Dir.pwd, filename)
 		puts "==> register file: #{filename}"
 		File.open(filename, "w+") { |f| 
 			f.write "# Kinecar built #{@timestamp}\n"
@@ -42,19 +135,26 @@ class ProjectRevision
 			f.write "\n\n"
 			f.write "|  application | base version | branch | commit |\n"
 			f.write "|:-------------|:------------:|:------:|:-------|\n"
-			#f.write self.method
+			@valid_projects_.each { |line| 
+				if !line.nil?
+					f.write "\| %s \| %s \| %s \| ``%s`` |\n" % [line[:name], line[:tag], line[:branch], line[:commit]]
+				end
+			}
+			
 
 
 			f.write "\n\n"
 			f.write "|  agl-service | base version | branch | commit |\n"
 			f.write "|:-------------|:------------:|:------:|:-------|\n"
-			#f.write self.method
+			
+
+			#f.write 
 		}
 	end
 
 	#private
 	def generate_report()
-		@valid_projects_.each { |e|  
+		@repository_projects_.each { |e|  
 			puts "==> #{e}"
 			Dir.chdir(e) {
 				commit  = %x[git rev-parse HEAD]
@@ -69,7 +169,19 @@ class ProjectRevision
 					puts "#{tag}"
 				end
 
+				project = Project.new(e.strip, branch.strip, commit.strip, tag.strip)
+				p project
+				if !project.nil?
+					if project.get_project.kind_of?(Array)
+						project.get_project.each {|element| @valid_projects_ << element }
+					else
+						puts "zzzz #{project.get_project}"
+						@valid_projects_ << project.get_project
+					end
+				end
 				#yield commit, branch, tag
+
+				puts @valid_projects_.length()
 			}
 		}
 	end
@@ -94,7 +206,7 @@ if __FILE__ == $0
 	ARGV.each do |a|
 		prr = ProjectRevision.new(a)
 		prr.scan_folders
-		#prr.generate_report
+		prr.generate_report
 		prr.register_file_on_disk
 	end
 end
